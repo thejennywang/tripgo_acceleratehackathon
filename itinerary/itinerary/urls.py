2a51b69f179e9e3645d5ebba2750474d71504019
Deniz Binay <deniz.binay@gmail.com>
2015-02-08 10:58:26 -0800

    pushing urls and new views.py

--- itinerary/itinerary/urls.py
+++ itinerary/itinerary/urls.py
@@ -8,6 +8,7 @@ import itineraryapp.views
 urlpatterns = patterns('',
     url(r'^$', itineraryapp.views.home, name='home'),
     url(r'^home.html$', itineraryapp.views.home, name='home'),
+    url(r'^mytrip.html$', itineraryapp.views.get_itinerary, name='get_itinerary'),
     url(r'^mongonaut/', include('mongonaut.urls')),
     url(r'^admin/', include(admin.site.urls)),   
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
