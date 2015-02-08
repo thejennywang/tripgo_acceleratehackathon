8e1b4773670e737b6e383e150f2e60dcf12fed2b
Deniz Binay <deniz.binay@gmail.com>
2015-02-08 12:02:43 -0800

    almost final views

--- itinerary/itineraryapp/views.py
+++ itinerary/itineraryapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from itineraryapp.models import City
+from itineraryapp.models import City, ThingsToDo
+import itinerary
 
 # Create your views here.
 def home(request):
@@ -22,29 +23,57 @@ def get_itinerary(request):
    
     days_duration = 14
 
-    adventure_cities = City.objects(region=location, characteristics="adventure").order_by('-ranking')
-    relax_cities = City.objects(region=location, characteristics="relax").order_by('-ranking')
+#     adventure_cities = City.objects(region=location, characteristics="adventure").order_by('-ranking')
+#  
+#     for city in adventure_cities:
+#         print(city.title + " " + str(city.characteristics))
 
-    for city in relevant_cities:
-        print(city.title + " " + str(city.characteristics))
 
-    if(days_duration == 14):
-        #we do something here
-        print("case 1")
-        sf = City.objects(title="San Francisco")
-        big_sur = City.objects(title="Big Sur")
-        la = City.objects(title="Los Angeles")
-        lv = City.objects(title="Las Vegas")
+    sf = City.objects(title="San Francisco").first()
+    big_sur = City.objects(title="Big Sur").first()
+    la = City.objects(title="Los Angeles").first()
+    lv = City.objects(title="Las Vegas").first()
+    gc = City.objects(title="Grand Canyon National Park").first()
+    
+    sb = City.objects(title="Santa Barbara").first()
+    sm = City.objects(title="Santa Monica").first()
+    sd = City.objects(title="San Diego").first()
+
+    sf.duration = 4
+    big_sur.duration = 1
+    la.duration = 3
+    lv.duration = 3
+    gc.duration = 3
+    
+    sb.duration = 5
+    sm.duration = 4
+    sd.duration = 5
+
+    sf.activities = ThingsToDo.objects(city=sf.id)
+    big_sur.activities = ThingsToDo.objects(city=big_sur.id)
+    la.activities = ThingsToDo.objects(city=la.id)
+    lv.activities = ThingsToDo.objects(city=lv.id)
+    gc.activities = ThingsToDo.objects(city=gc.id)
+    
+    sb.activities = ThingsToDo.objects(city=sb.id)
+    sm.activities = ThingsToDo.objects(city=sm.id)
+    sd.activities = ThingsToDo.objects(city=sd.id)
+
+    adventure_cities = [sf, big_sur, la, lv, gc]
+    relax_cities = [sb, sm, sd]
     
     if(days_duration == 7):
-        #we do something here
-        print("case 3")
-    
-    
+        sf.duration = 3
+        big_sur.duration = 1
+        la.duration = 3
+        adventure_cities = [sf, big_sur, la]
+        
+        sb.duration = 5
+        sm.duration = 2
+        relax_cities = [sb, sm]
     
     #sort by duration and ranking, then suggest places keeping duration limit 
-    result_city_list = relevant_cities
-    
-    context = {'itinerary_list': result_itinerary_list, 'duration': days_duration}
+    itinerary_list = [adventure_cities, relax_cities]
+    context = {'itinerary_list': itinerary_list, 'duration': days_duration}
 
     return render(request, 'mytrip.html', context)
